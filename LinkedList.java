41264c03c714a84115fe99b0cfb4bbf3cffbaca5
sairamcharanx <sairam.v99@gmail.com>
2023-08-09 21:07:24 +0530

    Node

--- LinkedList.java
+++ LinkedList.java
@@ -5,10 +5,22 @@ Node head;
 	class Node{
 	int data;
 	Node next;
-	}
+	
 
 	Node(int x){
 		data = x;
 		next = null;
 	}
+}
+
+public Node insertNode(int data){
+
+	//Calling the constructor with the passed argument to create a new Node
+	Node newNode = new Node(data);
+
+
+	
+
+	return newNode;
+}
 }
